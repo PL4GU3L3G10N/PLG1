8c564f364b1123a557de223f0a7c6e4eae2dd113
PL4GU3 <107338063+PL4GU3L3G10N@users.noreply.github.com>
2022-07-05 16:06:43 -0300

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,9 +1,1 @@
-#Scanner de Portas
-import socket
-
-cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente.connect(('bancocn.com', 80))
-cliente.send('restart\n\n')
-resposta = cliente.recv(1024)
-
-print resposta
+NOTHING HERE KK
